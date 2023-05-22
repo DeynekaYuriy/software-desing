ef931afa3beab878569001d26f8ce50a4961b9b6
Yuriy Deyneka <ipz213_dyum@student.ztu.edu.ua>
2023-05-22 14:00:00 +0200

    Lab 6 Program.cs fixed

--- lab-6/Program.cs
+++ lab-6/Program.cs
@@ -1,5 +1,4 @@
-﻿using BlackJack;
-using DesignPatterns.Mediator;
+﻿using DesignPatterns.Mediator;
 using lab_6.CoR;
 using lab_6.CoR.Handlers;
 using lab_6.Mediator.AirTrafficControl;
@@ -9,26 +8,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Console.WriteLine("1. CoR");
-        //var billingIssueHandler = new BillingIssueHandler(AnswerQuestion);
-        //var orderIssueHandler = new OrderIssueHandler(AnswerQuestion);
-        //var technicalIssueHandler = new TechnicalIssueHandler(AnswerQuestion);
-        //var securityIssueHandler = new SecurityIssueHandler(AnswerQuestion);
-        //billingIssueHandler.SetNext(orderIssueHandler).SetNext(technicalIssueHandler).SetNext(securityIssueHandler);
-        //billingIssueHandler.HandleQuestion();
-        //Console.WriteLine("2. Mediator");
-        //var boeing = new Aircraft("Boeing");
-        //var runways = new Dictionary<Runway, Aviation?>
-        //{
-        //    { new Runway(), null },
-        //    { new Runway(), null },
-        //};
-        //var commandCentre = new CommandCentre(runways);
-        //boeing.Land(commandCentre);
-        //boeing.TakeOff();
+        Console.WriteLine("1. CoR");
+        var billingIssueHandler = new BillingIssueHandler(AnswerQuestion);
+        var orderIssueHandler = new OrderIssueHandler(AnswerQuestion);
+        var technicalIssueHandler = new TechnicalIssueHandler(AnswerQuestion);
+        var securityIssueHandler = new SecurityIssueHandler(AnswerQuestion);
+        billingIssueHandler.SetNext(orderIssueHandler).SetNext(technicalIssueHandler).SetNext(securityIssueHandler);
+        billingIssueHandler.HandleQuestion();
+        Console.WriteLine("2. Mediator");
+        var boeing = new Aircraft("Boeing");
+        var runways = new Dictionary<Runway, Aviation?>
+        {
+            { new Runway(), null },
+            { new Runway(), null },
+        };
+        var commandCentre = new CommandCentre(runways);
+        boeing.Land(commandCentre);
+        boeing.TakeOff();
 
-        var game = new Game();
-        game.Start();
     }
     public static Answer AnswerQuestion(string question)
     {
